d57befab5f8b6e55955c464909f64c15ada1b85c
Davidstivenmoreno36912 <Davidstivenmoreno36912@gmail.com>
2025-09-29 08:27:32 -0500

    Update db.js

--- db.js
+++ db.js
@@ -11,7 +11,7 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  ssl: { rejectUnauthorized: true } // 👈 obligatorio en Aiven
+  ssl: { rejectUnauthorized: false } // 👈 obligatorio en Aiven
 });
 
 // Probar conexión
