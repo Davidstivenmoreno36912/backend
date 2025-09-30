967888e6dee841cbb991adc90c91a560c1be2a28
Davidstivenmoreno36912 <Davidstivenmoreno36912@gmail.com>
2025-09-30 15:12:13 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path');
 const mime = require('mime-types');
 const productRouter = require('./routes/products');
-const db = require('./db'); // 👈 usamos el pool con SSL
+const db = require('../db'); // 👈 usamos el pool con SSL
 
 const app = express();
 const PORT = process.env.PORT || 3000;
